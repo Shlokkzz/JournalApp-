e6eec5e2eb7234e6a2f7c21d9e3beeffd69acaa8
Shlokkzz <f20212441@goa.bits-pilani.ac.in>
2024-11-13 06:41:24 +0530

    EntryListFragment Add

--- app/src/main/java/androidsamples/java/journalapp/EntryListFragment.java
+++ app/src/main/java/androidsamples/java/journalapp/EntryListFragment.java
@@ -1,27 +1,115 @@
 package androidsamples.java.journalapp;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 /**
  * A fragment representing a list of Items.
  */
 public class EntryListFragment extends Fragment {
+  private static final String TAG = "EntryListFragment";
+  private JournalViewModel mJournalViewModel;
+  private View view;
+
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    mJournalViewModel = new ViewModelProvider(this).get(JournalViewModel.class);
+    // setHasOptionsMenu(true);
   }
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_entry_list, container, false);
+    view = inflater.inflate(R.layout.fragment_entry_list, container, false);
+
+    RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+    recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    JournalEntryListAdapter adapter = new JournalEntryListAdapter(getActivity());
+    recyclerView.setAdapter(adapter);
+
+    mJournalViewModel.getAllEntries().observe(requireActivity(),(List<JournalEntry> entries) -> adapter.setEntries(entries));
+
+    return view;
   }
 
+
+  private static class EntryViewHolder extends RecyclerView.ViewHolder {
+    private final TextView mTxtTitle;
+    private final TextView mTxtDate;
+    private final TextView mTxtStart;
+    private final TextView mTxtEnd;
+    private JournalEntry mEntry;
+
+    public EntryViewHolder(@NonNull View itemView) {
+      super(itemView);
+
+      mTxtTitle = itemView.findViewById(R.id.txt_item_title);
+      mTxtDate = itemView.findViewById(R.id.txt_item_date);
+      mTxtStart = itemView.findViewById(R.id.txt_item_start_time);
+      mTxtEnd = itemView.findViewById(R.id.txt_item_end_time);
+
+      // itemView.setOnClickListener(this::launchJournalEntryFragment);
+    }
+
+    void bind(JournalEntry entry) {
+      mEntry = entry;
+      this.mTxtTitle.setText(mEntry.title());
+      this.mTxtDate.setText(mEntry.date());
+      this.mTxtStart.setText(mEntry.start());
+      this.mTxtEnd.setText(mEntry.end());
+    }
+  }
+
+
+  private static class JournalEntryListAdapter extends RecyclerView.Adapter<EntryViewHolder>{
+
+    private final LayoutInflater mInflater;
+    private List<JournalEntry> mEntries;
+
+    public JournalEntryListAdapter(Context context) {
+      mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public EntryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View itemView = mInflater.inflate(R.layout.fragment_entry, parent, false);
+      return new EntryViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EntryViewHolder holder, int position) {
+      if (mEntries != null) {
+        JournalEntry current = mEntries.get(position);
+        // bind now
+        holder.bind(current);
+      }
+    }
+
+    @Override
+    public int getItemCount() {
+      return (mEntries == null) ? 0 : mEntries.size();
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setEntries(List<JournalEntry> entries) {
+      mEntries = entries;
+      notifyDataSetChanged();
+    }
+  }
 }
